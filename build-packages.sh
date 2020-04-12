cd0c36c1ab22e8f232aa460e3f2900d16cde205e
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-04-11 22:17:03 -0700

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -8,14 +8,14 @@ cd $DIR
 
 make_package() {
     cp $WORKSPACE/$1 $1
-    makepkg -s -d -p $1
+    makepkg -d -p $1
     rm -rf pkg src $1
 }
 
 yaourt_package() {
     yaourt -G $1
     cd $1
-    makepkg
+    makepkg -s
     cp $1*.pkg.* ..
     cd ..
     rm -rf $1
