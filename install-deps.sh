dcc654147b30b633978c018da641cef2305398b4
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-11 20:40:15 -0700

    fix

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 
 sudo pacman -S --noconfirm git base-devel openssh
-sudo chown user:user -R /home/user
+
+cp -r .ssh /home/user/.ssh
+sudo chown user:user -R /home/user/.ssh
+
+git config --global user.email "zasdfgbnm-bot@example.com"
+git config --global user.name "zasdfgbnm-bot"
