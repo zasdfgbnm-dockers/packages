3b62ab831b12a42c78b7737815970b4a94ed7346
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-11 20:44:52 -0700

    fix

--- install-deps.sh
+++ install-deps.sh
@@ -4,6 +4,8 @@ sudo pacman -S --noconfirm git base-devel openssh
 
 cp -r .ssh /home/user/.ssh
 sudo chown user:user -R /home/user/.ssh
+chmod 0600 /home/user/.ssh/id_rsa
+ssh-keyscan -t rsa github.com >> /home/user/.ssh/known_hosts
 
 git config --global user.email "zasdfgbnm-bot@example.com"
 git config --global user.name "zasdfgbnm-bot"
