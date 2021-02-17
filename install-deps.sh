f465dd74d2cf8933350be142ed48adf8f67d9a01
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-17 08:50:39 -0800

    save

--- install-deps.sh
+++ install-deps.sh
@@ -4,6 +4,7 @@ set -eux
 
 sudo pacman -Sy --noconfirm strace
 sudo pacman -Syu --noconfirm pacman
+sudo locale-gen
 ls -lah /var/lib/pacman/
 sudo strace pacman -Tv
 sudo pacman -Sy --noconfirm git base-devel openssh tree libffi xonsh
